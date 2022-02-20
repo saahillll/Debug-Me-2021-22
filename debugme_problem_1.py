7f23323da3861fb04205951ee67630666ace546e
Kaushik7558 <100069488+Kaushik7558@users.noreply.github.com>
2022-02-20 15:50:47 +0530

    Add files via upload

---
+++ debugme_problem_1.py
@@ -0,0 +1,12 @@
+"""
+PROBLEM 1:-
+MAX POINTS = 5
+EXPECTED OUTPUT = apple mango banana grapes kiwi
+"""
+
+fruit=("apple , mango, orange, banana, grapes")
+
+fruit.append("kiwi")
+
+for i in fruit
+    printf("i");

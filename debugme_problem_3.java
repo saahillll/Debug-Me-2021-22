90092494a4b54823b5c16e817f35c8fbf4b4f05f
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2022-02-28 21:46:06 +0530

    Create debugme_problem_3.java

---
+++ debugme_problem_3.java
@@ -0,0 +1,21 @@
+/*
+    Problem 3
+
+    Expected Output :
+            [hello,
+             , world]
+*/
+
+public class Problem4 {
+
+    public void main(String[] args) {
+
+        List<string> list = new List<string>();
+        list.append("hello");
+        list.append("/n");
+        list.append("world");
+
+        System.out.printf(list.toString());
+
+    }
+}

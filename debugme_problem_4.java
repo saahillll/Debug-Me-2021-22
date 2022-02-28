90d2d024a755fa1ba025869cf956cb5b80f20135
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2022-02-28 22:03:59 +0530

    Create debug-me problem 4

--- debugme_problem_4.java
+++ debugme_problem_4.java
@@ -1,1 +1,38 @@
+/*
+       Identify the problem here and debug the program.
+*/
+
+public class Problem4 {
+
+    class A{
+        void foo(){
+            System.out.println("Inside Class A");
+        }
+    }
+
+    class B extends A{
+
+        void foo(){
+            System.out.println("Inside Class B");
+        }
+    }
+
+    class C extends A{
+        void foo(){
+            System.out.println("Inside Class C");
+        }
+    }
+
+    class D extends B, C{
+        void foo(){
+            System.out.println("Inside Class D");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        D d = new D();
+        d.foo();
+    }
+}
 
